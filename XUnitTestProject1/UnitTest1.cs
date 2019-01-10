18583f63b595bf9c47b9f30f3ea1da1deb4b73fd
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-09 23:32:17 -0800

    I finished .cs. I have to complete the unit testing and add visual in README.

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,6 @@
 using System;
 using Xunit;
 
-namespace XUnitTestProject1
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
 
-        }
-    }
-}
+namespace AndrewTM
+{ }
